179120a1a78a695b22b984f29c4b83f16674da2e
eiselin <192197541+eiselin@users.noreply.github.com>
2026-03-05 15:47:05 +0100

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,206 @@
+-- Basic options
+vim.opt.number = true            -- Line numbers
+vim.opt.relativenumber = true    -- Relative line numbers
+vim.opt.cursorline = true        -- Highlight current line
+vim.opt.smartindent = true       -- Smart indentation
+vim.opt.wrap = false             -- Don't wrap lines
+vim.opt.undofile = true          -- Persistent undo
+vim.opt.clipboard = "unnamedplus" -- System clipboard integration
+vim.opt.termguicolors = true     -- True color support
+vim.opt.ignorecase = true        -- Case insensitive search
+vim.opt.smartcase = true         -- Case sensitive when uppercase present 
+vim.opt.mouse = "a"              -- Enable mouse support 
+vim.opt.tabstop = 2              -- 2 spaces for tabs 
+vim.opt.shiftwidth = 2           -- 2 spaces for indentation 
+vim.opt.expandtab = true         -- Use spaces instead of tabs 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+-- Plugins
+require("lazy").setup({
+
+  -- Colorscheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "latte",
+        transparent_background = true,
+        term_colors = true,
+        styles = {
+          comments = { "italic" },
+          keywords = { "italic" },
+          functions = {},
+          variables = {},
+        },
+        integrations = {
+          treesitter = true,
+          cmp = true,
+          telescope = { enabled = true },
+        },
+      })
+      vim.cmd("colorscheme catppuccin-latte")
+    end,
+  },
+  -- Treesitter for syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({ ensure_installed = { "typescript", "markdown", "python", "ruby", "html", "xml", "css", "lua"
+        },
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+  
+  -- Telescope (fuzzy finder)
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<esc>"] = actions.close,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
+          },
+          layout_strategy = "horizontal",
+          layout_config = {
+            width = 0.9,
+            height = 0.8,
+            prompt_position = "top",
+          },
+          sorting_strategy = "ascending",
+        },
+      })
+    end,
+  },
+
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+      -- Command line completion
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+          { name = "cmdline" },
+        },
+      })
+    end,
+  },
+
+})
+
+
+-- Key mappings
+-- Map jj to Escape in insert mode
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+-- Buffer navigation
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { noremap = true, silent = true })
+
+-- Quick save and quit
+vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
+
+-- No highlight search
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { noremap = true, silent = true })
+
+-- Move text up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- Keep cursor centered when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
---
+++ sync.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Source locations
+NVIM_DIR="$HOME/.config/nvim"
+ALACRITTY_DIR="$HOME/.config/alacritty"
+TMUX_CONF="$HOME/.tmux.conf"
+# Firefox profile with userChrome.css
+FIREFOX_CHROME_DIR="$HOME/Library/Application Support/Firefox/Profiles/8hvurr5f.default-release-1717767901382/chrome"
+
+echo "Syncing dotfiles into $DOTFILES_DIR ..."
+
+# --- nvim ---
+echo "  nvim"
+rm -rf "$DOTFILES_DIR/nvim"
+mkdir -p "$DOTFILES_DIR/nvim"
+cp "$NVIM_DIR/init.lua" "$DOTFILES_DIR/nvim/"
+[ -f "$NVIM_DIR/lazy-lock.json" ] && cp "$NVIM_DIR/lazy-lock.json" "$DOTFILES_DIR/nvim/"
+
+# --- alacritty (config only, themes are installed separately) ---
+echo "  alacritty"
+rm -rf "$DOTFILES_DIR/alacritty"
+mkdir -p "$DOTFILES_DIR/alacritty"
+cp "$ALACRITTY_DIR/alacritty.toml" "$DOTFILES_DIR/alacritty/"
+
+# --- tmux ---
+echo "  tmux"
+cp "$TMUX_CONF" "$DOTFILES_DIR/tmux/.tmux.conf"
+
+# --- firefox userChrome.css ---
+echo "  firefox userChrome.css"
+if [ -f "$FIREFOX_CHROME_DIR/userChrome.css" ]; then
+  cp "$FIREFOX_CHROME_DIR/userChrome.css" "$DOTFILES_DIR/firefox/userChrome.css"
+else
+  echo "    WARNING: userChrome.css not found at expected path"
+fi
+
+echo "Done."
